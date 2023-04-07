82d9c96f147bfca7b11e8d485d3dae75d49e606a
HusnainYusufi <102365537+HusnainYusufi@users.noreply.github.com>
2023-04-08 00:01:05 +0500

    Update userApi.js

--- routes/userApi.js
+++ routes/userApi.js
@@ -240,13 +240,13 @@ app.post('/myProfile' , (req , res) =>{
                     return res.json(handleErr(err))
                 }else{
                     if(doc !== null){
-                        community.find({_id:doc_id})
+                        community.find({_id:doc._id})
                         .exec((err2 , comm) =>{
                             if(err2){
                                 return res.json(handleErr(err2))
                             }else{
                                 if(comm !== null){
-                                    Preference.find({_id:doc_id})
+                                    Preference.find({_id:doc._id})
                                     .exec((err3 , pref) =>{
                                         if(err3){
                                             return res.json(handleErr(err))
