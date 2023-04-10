22f3ed62f87c0e086b86b26469149a58218523eb
HusnainYusufi <102365537+HusnainYusufi@users.noreply.github.com>
2023-04-10 22:20:50 +0500

    new

--- routes/preferenceApi.js
+++ routes/preferenceApi.js
@@ -20,21 +20,60 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.mongo.ObjectId
 
 
-app.post('/addPreference' , (req , res) =>{
-    if(req.body.uid !== undefined ){
+// app.post('/addPreference' , (req , res) =>{
+//     if(req.body.uid !== undefined ){
+//         const data = req.body;
+//         Preference.create(data , (err , doc) =>{
+//             if(err){
+//                 return res.json(handleErr(err))
+//             }else{
+//                 return res.json(handleSuccess(doc));
+//             }
+//         })
+//         }else{
+//             return res.json(handleErr("UID is required"))
+//         }
+// })
+
+//add update
+app.post('/addPreference:id' , (req , res) =>{
+    const sid = req.params.id;
+
+    if(sid !== undefined){
         const data = req.body;
-        Preference.create(data , (err , doc) =>{
+    
+        Preference.findOne({uid:sid})
+        .exec((err , doc) =>{
             if(err){
                 return res.json(handleErr(err))
             }else{
-                return res.json(handleSuccess(doc));
+
+                if(doc !== null){       
+                    console.log(data);             
+                    Preference.findOneAndUpdate({uid:sid} , data , {new:true})
+                    .exec((err2 , doc3) =>{
+                        if(err){
+                            return res.json(handleErr(err2))
+                        }else{
+                            return res.json(handleSuccess(doc3));
+                        }
+                    })
+
+                }else{
+                    Preference.create(data , (err , doc4) =>{
+                        if(err){
+                            return res.json(handleErr(err))
+                        }else{
+                            return res.json(handleSuccess(doc4));
+                        }
+                    })
+                }
             }
         })
-        }else{
-            return res.json(handleErr("UID is required"))
-        }
+    }else{
+        return res.json(handleErr("UID is required"));
+    }
 })
-
 
 
 
