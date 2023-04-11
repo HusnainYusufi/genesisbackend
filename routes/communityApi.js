0d990b681ff269252002f469420b0f67380c6d8f
HusnainYusufi <102365537+HusnainYusufi@users.noreply.github.com>
2023-04-11 09:27:19 +0500

    new

--- routes/communityApi.js
+++ routes/communityApi.js
@@ -20,21 +20,61 @@ const mongoose = require("mongoose");
 const ObjectId = mongoose.mongo.ObjectId
 
 //add community
+// app.post('/addCommunity' , (req , res) =>{
+//     if(req.body.uid !== undefined ){
+//         let data = req.body;
+//         Community.create(data , (err , doc) =>{
+//             if(err){
+//                 return res.json(handleErr(err))
+//             }else{
+//                 return res.json(handleSuccess(doc));
+//             }
+//         })
+//         }else{
+//             return res.json(handleErr("UID is required"))
+//         }
+// })
 
-app.post('/addCommunity' , (req , res) =>{
-    if(req.body.uid !== undefined ){
-        let data = req.body;
-        Community.create(data , (err , doc) =>{
+
+app.post('/addCommunity:id' , (req , res) =>{
+    const sid = req.params.id;
+
+    if(sid !== undefined){
+        const data = req.body;
+    
+        Community.findOne({uid:sid})
+        .exec((err , doc) =>{
             if(err){
                 return res.json(handleErr(err))
             }else{
-                return res.json(handleSuccess(doc));
+
+                if(doc !== null){       
+                    console.log(data);             
+                    Community.findOneAndUpdate({uid:sid} , data , {new:true})
+                    .exec((err2 , doc3) =>{
+                        if(err){
+                            return res.json(handleErr(err2))
+                        }else{
+                            return res.json(handleSuccess(doc3));
+                        }
+                    })
+
+                }else{
+                    data.uid = sid;
+                    console.log(data);
+                    Community.create(data , (err , doc4) =>{
+                        if(err){
+                            return res.json(handleErr(err))
+                        }else{
+                            return res.json(handleSuccess(doc4));
+                        }
+                    })
+                }
             }
         })
-        }else{
-            return res.json(handleErr("UID is required"))
-        }
+    }else{
+        return res.json(handleErr("UID is required"));
+    }
 })
-
 
 module.exports = app;
