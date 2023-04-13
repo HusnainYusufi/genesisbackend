599b16fa8537bc54a7046da8688c59595ea843a8
HusnainYusufi <102365537+HusnainYusufi@users.noreply.github.com>
2023-04-13 07:30:29 +0500

    new

--- routes/chatApi.js
+++ routes/chatApi.js
@@ -29,7 +29,7 @@ app.post('/getChat', (req, res) => {
             sender,
             receiver
         }
-        Chat.find({ $or: [{ sender: sender }, { receiver: sender } , {sender : receiver} , {receiver : receiver}] }).populate('sender receiver').exec((err, doc) => {
+        Chat.find({ $or: [{ sender: sender }, {receiver : receiver} , {sender : receiver} , {receiver : sender}] }).populate('sender receiver').exec((err, doc) => {
             if (err) {
                 console.log(err);
             }else {
